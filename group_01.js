4433d05094464f1f5fe2343edfc5ca9e2b313d90
Maetz007 <maetz007@yahoo.com>
2016-05-26 17:46:33 -0500

    Initial Commit

--- group_01.js
+++ group_01.js
@@ -4,3 +4,62 @@ var boo = ["Boo", "11435", "54000", 3];
 var scout = ["Scout", "6243", "74750", 5];
 
 var employees = [atticus, jem, boo, scout];
+
+
+
+
+var employeeRecord = function() {
+
+  for (var i = 0; i < employees.length; i++) {
+
+    var name = employees[i][0];
+    var employeeNumber = employees[i][1];
+    var salary = parseInt(employees[i][2]);
+    var review = employees[i][3];
+    var adjustedSalary = 0;
+    var bonus = 0;
+    var roundedBonus = 0;
+
+    if (employeeNumber.length == 4) {
+      bonus += 0.05;
+    }
+
+    if(salary >= 65000){
+      bonus -= 0.01;
+    }
+
+    switch (review) {
+      case 0:
+      case 1:
+      case 2:
+        bonus = 0;
+        break;
+      case 3:
+        bonus += 0.04;
+        break;
+      case 4:
+        bonus += 0.06;
+        break;
+      case 5:
+        bonus += 0.1;
+        break;
+      default:
+    }
+
+    if (bonus > 0.13){
+      bonus = 0.13;
+    }
+
+    adjustedSalary = salary + (salary * bonus);
+    roundedBonus = Math.round(salary * bonus);
+
+    var newArray = [name, bonus, adjustedSalary, roundedBonus];
+
+    console.log(newArray);
+
+  }
+};
+
+employeeRecord(employees);
+
+//Everything works to this point.
